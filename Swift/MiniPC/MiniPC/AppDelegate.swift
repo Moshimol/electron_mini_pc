9e3b5c4bf0fc48b0c199b42ffc01d7cdc8f9b217
lushitong <lushitong@qq.com>
2021-01-18 19:53:17 +0800

    初始化项目 初始化electron

---
+++ Swift/MiniPC/MiniPC/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MiniPC
+//
+//  Created by lushitong on 2021/1/18.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
